a88965aab4499aa3ca5c4140a8d5e827ebefbacd
Willian Schmitt <w.schmitt2017@gmail.com>
2025-09-08 09:43:01 -0300

    resolved error in remove scroll-button

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       />
 
       <Header />
-      <Description targetId="secao2" />
+      <Description />
       <Carousel />
       <InfoButtons />
     </main>
--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 export function Description() {
   return (
     <div className="max-w-4xl flex flex-col items-center gap-6">
